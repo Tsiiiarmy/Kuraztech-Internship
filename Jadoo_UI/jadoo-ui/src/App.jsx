98e477eabcc88e5131c6d57fb95e2bd612b18dcf
Tsion Getachew <Tsiiiarmy@gmail.com>
2025-07-21 17:48:00 +0300

    tailwind is aliveeee

---
+++ Jadoo_UI/jadoo-ui/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-4xl font-bold text-center text-pink-600 mt-20">
+      Tailwind is ALIVEEE 🎉🔥
+    </div>
+  );
+}
+
+
+export default App
